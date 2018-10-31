53f51cfbabe618a9dc719426405c8241e76c5bcf
bruceleeon <tac@computingondemand.com>
2018-10-31 07:56:04 -0400

    Update config.js

--- website_example/config.js
+++ website_example/config.js
@@ -3,6 +3,7 @@ var api = "http://poolhost:8117";
 var email = "support@poolhost.com";
 var telegram = "https://t.me/YourPool";
 var discord = "https://discordapp.com/invite/YourPool";
+var webw = "https://wallet.italocoin.com/#!/";
 
 var marketCurrencies = ["{symbol}-BTC", "{symbol}-USD", "{symbol}-EUR", "{symbol}-CAD"];
 
